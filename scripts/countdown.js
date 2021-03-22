e96e4d42060479a542e21d30706cead49259ed34
Keven Molina <kevenmolina@gmail.com>
2021-03-22 09:26:38 -0500

    reduced repetive timeUnit assignment to a function call

--- scripts/countdown.js
+++ scripts/countdown.js
@@ -1,37 +1,68 @@
 'use strict '  
 //DOM Element 
+document.querySelector('h1').innerText = "We're moving soon"
+
 
 const timer = document.querySelector('#time-container');
-const timeCards = document.querySelectorAll('.time-card h2');
+const timeCardUnits = document.querySelectorAll('.time-card h2');
+const timeCardTitles = document.querySelectorAll('.time-card h3');
 //Get the Date 14 days into the future, or based on the date user inputs
 // const deadLine =new Date(Math.floor(new Date().setDate(new Date(startTime).getDate()+14)))
-const deadLine =new Date("Apr 3, 2021, 00:00:00");
+
+const deadLine =new Date("Oct 30, 2021, 00:00:00");
+
+// const startTime = new Date().getTime();
+// let timeRemaining =  deadLine - startTime;
+// let timeUnits={
+//   "days":Math.floor(timeRemaining/(1000*60*60*24)),
+//   "hours":Math.floor(timeRemaining%(1000*60*60*24)/(1000*60*60)),
+//   "minutes": Math.floor(timeRemaining%(1000*60*60)/(1000*60)),
+//  "seconds":Math.floor(timeRemaining%(1000*60)/1000)
+// };
+
+function assignTimeUnitValue(obj,domElem,title){
+ let cardTitle=title.innerText.toLowerCase();
+ console.log(cardTitle)
+  switch (cardTitle) {
+    case "days":
+      console.log(`assigning time unit:${cardTitle}`)
+         domElem.innerText=obj.days
+      break;
+    case "hours":
+      console.log(`assigning time unit:${cardTitle}`)  
+       domElem.innerText=obj.hours
+      break;
+    case "minutes":
+      console.log(`assigning time unit:${cardTitle}`)
+       domElem.innerText=obj.minutes
+      break;
+    case "seconds":
+      console.log(`assigning time unit:${cardTitle}`)
+       domElem.innerText=obj.seconds
+      break;
+    default:
+      alert("Unfortunately, this entry time does not exist.")
+      break;
+  }
+}
+
 setInterval( ()=>{
   const startTime = new Date().getTime();
   let timeRemaining =  deadLine - startTime;
-
+// console.log(startTime)
 //Time units(This could be refactored into a function call)
-let days = Math.floor(timeRemaining/(1000*60*60*24));
-let hours = Math.floor(timeRemaining%(1000*60*60*24)/(1000*60*60));
-let minutes = Math.floor(timeRemaining%(1000*60*60)/(1000*60));
-let seconds = String(Math.floor(timeRemaining%(1000*60)/1000));
+let timeUnits={
+  "days":Math.floor(timeRemaining/(1000*60*60*24)),
+  "hours":Math.floor(timeRemaining%(1000*60*60*24)/(1000*60*60)),
+  "minutes": Math.floor(timeRemaining%(1000*60*60)/(1000*60)),
+ "seconds":Math.floor(timeRemaining%(1000*60)/1000)
+};
 
-//Timer update
-  //Change text to updated time
-// console.log(seconds)
-// console.log(minutes)
-//  let someFormattedDate = `
-//  -------------------------------------------------------------------------------
-// // | ${days} Day Left | ${hours} Hours left | ${minutes} Minutes Left | ${seconds} Seconds left |
-//  -------------------------------------------------------------------------------
-//  `;
-//  console.log(someFormattedDate)
-// console.log(
-  timeCards[0].innerText=days;
-  timeCards[1].innerText=hours;
-  timeCards[2].innerText=minutes;
-timeCards[3].innerText=seconds;
-// )
+for (let i = 0; i < timeCardTitles.length; i++) {
+  console.log(i)
+  assignTimeUnitValue(timeUnits,timeCardUnits[i],timeCardTitles[i]); 
+}
+
 },1000);
 
 console.log(timer)
